79641b98acbee5feba7ea42a2e33624a9201526e
Troy Tung <troytung@gmail.com>
2016-12-14 02:44:16 +0800

    add line messaging api

--- src/main/java/com/playbot/PlaybotApplication.java
+++ src/main/java/com/playbot/PlaybotApplication.java
@@ -2,11 +2,20 @@ package com.playbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//@SpringBootApplication
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@SpringBootApplication
+@LineMessageHandler
 @Controller
 @EnableAutoConfiguration
 public class PlaybotApplication {
@@ -14,10 +23,25 @@ public class PlaybotApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "play with Line's Messaging API";
+    }
+
+    @EventMapping
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        return new TextMessage(event.getMessage().getText());
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
     }
 
     public static void main(String[] args) {
+        System.setProperty("line.bot.channelToken",
+                "Xsea4Shuu4mppQrCsvygtCfHCXFZOjeHYT7J/mTkxFwmdkoVIM4ZYF5CMZnSP5sX3z6p+a8bjBA0mZ2aMHH2bpWbv5e7sRfOTO1zc9LyzdN/MhU0kaibFb4hGhQJ7npsZIcf8AesPkHCfwhVD8TxsgdB04t89/1O/w1cDnyilFU=");
+        System.setProperty("line.bot.channelSecret", "3ddbaba52ebbc09fa38defac7ef35516");
+
         SpringApplication.run(PlaybotApplication.class, args);
     }
 
